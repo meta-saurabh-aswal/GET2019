d22598ee99972a486e31d311dc0dc0bd7e393884
meta-saurabh-aswal <46713118+meta-saurabh-aswal@users.noreply.github.com>
2019-01-23 14:39:48 +0530

    With comments

--- Assignment4/ArrOperations.java
+++ Assignment4/ArrOperations.java
@@ -1,7 +1,27 @@
 import java.util.*;
 
+/*
+ArrOperations class is to do four operations on an array
+Operations:
+1. To find the maximum mirror sequence.
+2. To split the array into 2 parts such that the sum of the elements
+ of both parts is equal.
+3. Return the number of clumps in the array, ie. same element occuring
+ adjacent to itself.
+4. Swap the next element of x value with y value.
+
+The ArrOperationsTest class is being used to test the ArrOperatins classes' methods
+using different test cases with anotations and methods provided by JUnit.
+*/
 public class ArrOperations
 {
+	
+/*
+To find the largest mirror sequence in the array.
+
+@param array input as int.
+@return the length of largest mirror sequence as int.
+*/	
 	int maxMirror(int arr[])
 	{
 		if(arr == null){
@@ -11,7 +31,7 @@ public class ArrOperations
 		int len = arr.length;
 		int tempCount = 1;
 		int count = 1;
-		int[] revArr = new int[len];
+		int[] revArr = new int[len];			//Taking a reverse array for comparing.
 		
 		for(int i=0; i<len; i++)
 		{
@@ -29,16 +49,21 @@ public class ArrOperations
 				}				
 			}
 			
-			if(tempCount > count)
+			if(tempCount > count)			//Storing the largest sequence in count.
 			{
 				count = tempCount;
-				tempCount = 1;
+				tempCount = 1;			//resetting the value of count.
 			}
 		}
 		return count;
 	}
 	
-	
+/*
+To find the number of clumps int the array.
+
+@param array input as int.
+@return the total number of clumps as int.
+*/
 	int clumps(int arr[])
 	{
 		if(arr == null){
@@ -49,14 +74,20 @@ public class ArrOperations
 		
 		for(int i=1; i < arr.length-1; i++)
 		{
-			if(arr[i] == arr[i-1] && (arr[i] != arr[i+1]) || i+1 == arr.length-1)
+			if(arr[i] == arr[i-1] && (arr[i] != arr[i+1]) || i+1 == arr.length-1)		//Checking the current value with its previous and next value to ignore counting the sequence over and over.
 			{
 				count++;
 			}
 		}
 		return count;
 	}
-	
+/*
+To swap the values of element next to x with y.
+
+@param input int array.
+@param values of x and y as int.
+@return fixed array as int.
+*/
 	int[] fixXY(int arr[], int x, int y)
 	{
 		if(arr == null){
@@ -89,7 +120,12 @@ public class ArrOperations
 		}
 		return arr;
 	}
-	
+/*
+To split the array into two parts where the sum of elements of both parts is equal.
+
+@param input array as int.
+@return -1 if the array can't be split or index of the splitting point.
+*/
 	int splitArray(int arr[])
 	{
 		if(arr == null){
