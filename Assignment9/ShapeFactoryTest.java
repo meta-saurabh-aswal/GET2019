802aba45eb2d8129509e846b7bd8968048ceaae9
meta-saurabh-aswal <46713118+meta-saurabh-aswal@users.noreply.github.com>
2019-02-05 17:03:16 +0530

    Updated tests.

--- Assignment9/ShapeFactoryTest.java
+++ Assignment9/ShapeFactoryTest.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ShapeFactoryTest
@@ -12,10 +16,18 @@ public class ShapeFactoryTest
 		double rParams[] = new double[] {5, 4, 2, 3};	
 		screenObj.addShape("rectangle", rParams);
 		
-		//Circle
+		//Circle {X coordinate of origin, Y coordinate, radius of the circle}
 		double cParams[] = new double[] {4, 5, 3};
 		screenObj.addShape("circle", cParams);
 		
+		//Square {X coordinate of origin, Y coordinate, side of the square}
+		double sParams[] = new double[] {3, 3, 8};
+		screenObj.addShape("square", sParams);
+		
+		//Triangle {X coordinate of origin, Y coordinate, sides of the triangle}
+		double tParams[] = new double[] {2, 3, 10, 50, 80};
+		screenObj.addShape("triangle", tParams);
+
 		screenObj.deleteShape("rectangle", 1);
 	}
 	
@@ -23,6 +35,7 @@ public class ShapeFactoryTest
 	public void testEnclosed()
 	{	
 		Screen screenObj = Screen.getScreenObject();
+		List<Shape> enclosers = new ArrayList<Shape>();	
 		
 		//@params {X coordinate of origin, Y coordinate, length of the rectangle, width}
 		double rParams[] = new double[] {5, 4, 2, 3};	
@@ -32,7 +45,7 @@ public class ShapeFactoryTest
 		double cParams[] = new double[] {6, 5, 3};
 		screenObj.addShape("circle", cParams);
 		
-		Point p = new Point(6,4);
-		screenObj.enclosingShapes(p);
+		Point p = new Point(6,5);
+		enclosers = screenObj.enclosingShapes(p);	
 	}
 }
